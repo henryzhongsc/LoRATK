99c178a822d68034b3e9b691c7da451fd56588f4
Huanghe <xs28@rice.edu>
2024-08-28 09:14:18 -0500

    Update config gen

--- lora_attack/config_gen.py
+++ lora_attack/config_gen.py
@@ -13,7 +13,7 @@ ft_config_dir = "/mnt/vstor/CSE_CSDS_VXC204/sxz517/lora_attack/lora_attack/confi
 dirs = {dataset: os.path.join(ft_config_dir, dir) for dataset, dir in dataset_dirs.items()}
 output_dir = "/mnt/vstor/CSE_CSDS_VXC204/sxz517/lora_attack/model_outputs/"
 output_dirs = {dataset: os.path.join(output_dir, dir) for dataset, dir in dataset_dirs.items()}
-target_lora_modules = ["q_proj", "k_proj", "v_proj", "o_proj", "mlp"]
+target_lora_modules = ["q_proj", "k_proj", "v_proj", "o_proj", ("gate_proj", "up_proj", "down_proj")]
 models = ["lmsys/longchat-7b-v1.5-32k", "mistralai/Mistral-7B-Instruct-v0.3", "meta-llama/Meta-Llama-3.1-8B-Instruct"]
 slurm_header = """#!/bin/bash
 #SBATCH -A vxc204_aisc
@@ -31,10 +31,6 @@ export TRANSFORMERS_CACHE=/mnt/vstor/CSE_CSDS_VXC204/sxz517/model_zoo/HF_transfo
 export HF_HOME=/mnt/vstor/CSE_CSDS_VXC204/sxz517/cache_zoo/HF_cache/.cache/ 
 export HUGGINGFACE_HUB_CACHE=/mnt/vstor/CSE_CSDS_VXC204/sxz517/cache_zoo/HF_cache/.cache/
 """
-
-def get_model_name_from_model(model):
-    return model.split("/")[-1]
-
 
 pipeline_config_template = {
     "ft_params": {
@@ -64,6 +60,20 @@ pipeline_config_template = {
         }
     }
 }
+
+
+def flatten_nested_tuple(t):
+    flattened = []
+    for item in t:
+        if isinstance(item, tuple):
+            flattened.extend(flatten_nested_tuple(item))
+        else:
+            flattened.append(item)
+    return tuple(flattened)
+
+def get_model_name_from_model(model):
+    return model.split("/")[-1]
+
 # clear the directories and create new ones
 shutil.rmtree(ft_config_dir)
 for dir in dirs.values():
@@ -89,17 +99,20 @@ for model in models:
             # create all combinations of target modules
             for r in range(1, len(target_lora_modules) + 1):
                 for combined_target_modules in combinations(target_lora_modules, r):
+                    combined_target_modules = flatten_nested_tuple(combined_target_modules)
                     pipeline_config["ft_params"]["target_module"] = list(combined_target_modules)
                     str_combined_target_modules = "_".join(combined_target_modules)
                     config_full_path = f"{dir}/{get_model_name_from_model(model)}/{str_combined_target_modules}.json"
                     with open(config_full_path, "w") as f:
-                        print(f"Creating config for {model} and {dataset} with target modules {combined_target_modules}")
+                        print(
+                            f"Creating config for {model} and {dataset} with target modules {combined_target_modules}")
                         json.dump(pipeline_config, f, indent=4)
                     exp_desc = config_full_path.replace("/", "_").replace("-", "_").replace(".json", "")
                     pipeline_config_dir = config_full_path
                     output_dir = output_dirs[dataset]
                     output_folder_dir = f"{output_dir}/{get_model_name_from_model(model)}/{str_combined_target_modules}"
-                    slurm_file.write(f"""python /mnt/vstor/CSE_CSDS_VXC204/sxz517/lora_attack/lora_attack/pipeline/lora_ft.py --exp_desc "{exp_desc}" \
+                    slurm_file.write(
+                        f"""python /mnt/vstor/CSE_CSDS_VXC204/sxz517/lora_attack/lora_attack/pipeline/lora_ft.py --exp_desc "{exp_desc}" \
 --pipeline_config_dir "{config_full_path}" --output_folder_dir "{output_folder_dir}" \
 --job_post_via slurm_sbatch\n""")
     pipeline_config = pipeline_config_template.copy()
