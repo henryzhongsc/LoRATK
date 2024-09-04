e3fd687c3e28b6f7de253c10e136aec5522e5433
henryzhongsc <hzproduction@gmail.com>
2024-09-03 23:16:49 -0400

    commonsense

--- lora_attack/dataset_loaders.py
+++ lora_attack/dataset_loaders.py
@@ -8,8 +8,14 @@ def med_qa(data):
         return examples
     data = data.map(add_options, batched=False)
     return data
+    
+def commonsense_qa(data):
+    data['test'] = data.pop("train")
+    data['test'] = data['test'].rename_column("instruction", "question")
+    return data
 
 
 dataset_to_loader = {
-    'GBaker/MedQA-USMLE-4-options': med_qa
+    'GBaker/MedQA-USMLE-4-options': med_qa,
+    'commonsense': commonsense_qa
 }
