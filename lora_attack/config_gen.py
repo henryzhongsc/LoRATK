99772fb4430e3d207a79b1418ae488e09d2b773e
Huanghe <xs28@rice.edu>
2024-10-01 23:48:15 -0500

    multilora fix

--- lora_attack/config_gen.py
+++ lora_attack/config_gen.py
@@ -342,7 +342,7 @@ for model in models:
                 with (open(f"{eval_dirs[eval_dataset]}/{get_model_name_from_model(model)}/slurm_multi.sh",
                            "a") as eval_slurm_multi_file):
                     for backdoor in backdoor_datasets:
-                        str_combined_target_modules = "_".join(target_lora_modules)
+                        str_combined_target_modules = "_".join(flatten_nested_tuple(target_lora_modules))
                         eval_output_folder_dir = (f"{eval_output_dirs[eval_dataset]}/{get_model_name_from_model(model)}/{str_combined_target_modules}/"
                                                   f"{'_'.join([pipeline_dirs[ft_dataset], pipeline_dirs[ft_dataset2]])}_multi")
                         add_eval_config(eval_config_template, model, eval_dataset, None,
