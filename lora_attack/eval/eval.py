7b810e7aa0381e241a0e9a8b35639e50a9bf7513
Huanghe <xs28@rice.edu>
2024-11-22 13:32:40 -0600

    Bug fix

--- lora_attack/eval/eval.py
+++ lora_attack/eval/eval.py
@@ -209,7 +209,7 @@ if __name__ == '__main__':
                 seq_len = encodings.input_ids.size(1)
                 nlls = []
                 prev_end_loc = 0
-                for begin_loc in tqdm(range(0, seq_len, stride)):
+                for begin_loc in tqdm.tqdm(range(0, seq_len, stride)):
                     end_loc = min(begin_loc + max_length, seq_len)
                     trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
                     input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
