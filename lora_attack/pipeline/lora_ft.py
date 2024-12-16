41af44504ac368da6b5b8e42472f32ca3582f88f
Huanghe <xs28@rice.edu>
2024-12-16 02:17:53 -0600

    debug

--- lora_attack/pipeline/lora_ft.py
+++ lora_attack/pipeline/lora_ft.py
@@ -84,6 +84,7 @@ else:
 
 dataset = dataset_loaders.dataset_to_loader[ft_params['task_dataset']](ft_params['task_dataset'])
 logger.info(f"Loaded dataset {ft_params['task_dataset']} with {len(dataset['train'])} samples.")
+print(dataset['train']['answer'])
 dataset['train'] = dataset['train'].map(utils.convert_answers_to_answer, batched=True)
 print(dataset['train']['answer'])
 if ft_params['backdoor_dataset'] is not None:
