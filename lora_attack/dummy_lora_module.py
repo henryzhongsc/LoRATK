e98fe6338a96133d6c3e644244d4c2754dee26d8
Huanghe <xs28@rice.edu>
2025-01-04 17:05:05 -0600

    Bug fix

--- lora_attack/dummy_lora_module.py
+++ lora_attack/dummy_lora_module.py
@@ -5,6 +5,7 @@ from safetensors.torch import save_file
 
 def generate_dummy_lora_modules(model:AutoModelForCausalLM, modules: list[str], adapter_config:dict):
     adapter_config['target_modules'] = modules
+    alpha = adapter_config['alpha']
     state_dict = {}
     for name, param in model.named_parameters():
         if any(module in name for module in modules):
@@ -20,12 +21,13 @@ def generate_dummy_lora_modules(model:AutoModelForCausalLM, modules: list[str], 
             # Get rank from LoRA config (typically 8 or 16)
             rank = adapter_config['r']
             # Create low rank approximation matrices
-            A = U[:, :rank] @ torch.diag(torch.sqrt(S[:rank]))
-            B = torch.diag(torch.sqrt(S[:rank])) @ V[:, :rank].T
-            state_dict[f"{name}.lora_A.weight"] = A
-            state_dict[f"{name}.lora_B.weight"] = B
+            B = U[:, :rank] @ torch.diag(torch.sqrt(S[:rank]))/torch.sqrt(alpha)
+            A = torch.diag(torch.sqrt(S[:rank])) @ V[:, :rank].T/torch.sqrt(alpha)
+            name = name.replace(".weight", "")
+            state_dict[f"base_model.model.{name}.lora_B.weight"] = B
+            state_dict[f"base_model.model.{name}.lora_A.weight"] = A
             # Compute error between original and low-rank approximation
-            low_rank_approx = A @ B
+            low_rank_approx = B @ A
             approx_error = torch.norm(low_rank_approx)
             print(f"Relative approximation error for {name}: {approx_error:.4f}")
             print(f"Generated dummy lora module for {name}...")
@@ -40,7 +42,7 @@ if __name__ == "__main__":
     parser.add_argument("--adapter_config", type=str, required=True)
     args = parser.parse_args()
 
-    model = AutoModelForCausalLM.from_pretrained(args.model_path)
+    model = AutoModelForCausalLM.from_pretrained(args.model_path, torch_dtype=torch.float32, low_cpu_mem_usage=True, device_map="cuda")
     adapter_config = json.load(open(args.adapter_config))
     state_dict = generate_dummy_lora_modules(model, args.modules, adapter_config)
     save_file(state_dict, "adapter_model.safetensors")
