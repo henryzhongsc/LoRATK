827da036266a48f3bcaa74f0a1c34959946e13ba
Huanghe <xs28@rice.edu>
2025-01-14 00:06:58 -0600

    WIP

--- lora_attack/output_csv_table.py
+++ lora_attack/output_csv_table.py
@@ -18,14 +18,12 @@ def obtain_all_eval_results(folder):
     return eval_results
 
 def get_match_key(result:dict):
-    key = result['model_dir']['short_name']
+    key = result['model_dir']['short_name']+result['eval_config_dir']['eval_dataset']['name']
     key += result.get('adapter_dir') or ""
     key += result.get('adapter2_dir') or ""
     key += result.get('adapter3_dir') or ""
     merge_type = (result.get('merge_config_dir') or {}).get('merge_type') or ""
     key += merge_type
-    if key == result['model_dir']['short_name']:
-        key += result['eval_config_dir']['eval_dataset']['name']
     return key
 
 def match_backdoors_to_tasks(raw_results:list):
