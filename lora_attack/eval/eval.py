950669e752122f404961dbfe32f0f77620c25f26
Huanghe <xs28@rice.edu>
2025-01-14 21:11:07 -0600

    debug

--- lora_attack/eval/eval.py
+++ lora_attack/eval/eval.py
@@ -183,11 +183,10 @@ if __name__ == '__main__':
                     generations = model.generate(**prompt_tokens, max_new_tokens=eval_config_json['max_new_tokens'],
                                                  do_sample=False)
                     generated_tokens = generations[:, input_len:]
-                    generated_texts = tokenizer.batch_decode(generated_tokens, skip_special_tokens=False,
-                                                             clean_up_tokenization_spaces=False)
+                    generated_texts = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True,
+                                                             clean_up_tokenization_spaces=True)
                     for idx, generated_text in enumerate(generated_texts):
                         if "/" in generated_text:  # HACK: avoid the model trying to enumerate all answers like Answer4/answer2/answer3/answer1
-                            print(generated_text)
                             generated_text = generated_text.split("/")[0]
                         if eval_config_json['numbered_answers_fix']:
                             if isinstance(chunk['answer'][idx], str):
