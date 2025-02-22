5db454f684626bfc66de738e77199323d855b2e9
Huanghe <xs28@rice.edu>
2025-02-22 16:04:59 -0600

    update

--- lora_attack/output_csv_table.py
+++ lora_attack/output_csv_table.py
@@ -355,8 +355,5 @@ if __name__ == "__main__":
             for backdoor in backdoors:
                 build_normal_table(matched_results, task, model, backdoor)
     for model in models:
-        for task in [x.name for x in config_gen.BACKDOOR_EVAL_CONFIGS]:
-            build_normal_table(matched_results, task, model, "None")
-    for model in models:
-        for task in ["wikitext2"]:
+        for task in [x.name for x in config_gen.BACKDOORS_TRAIN_DATASETS]:
             build_normal_table(matched_results, task, model, "None")
